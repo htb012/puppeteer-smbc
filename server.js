58c1f549a45912ccf520b1a0387f72b81a983a4e
embokoir <30351534+embokoir@users.noreply.github.com>
2020-05-06 21:43:41 +0900

    beautify indent

--- server.js
+++ server.js
@@ -18,50 +18,50 @@ const ACCOUNT_NUMBER = process.env.ACCOUNT_NUMBER
 const PASSWORD = process.env.PASSWORD
 
 async function fetchBalance()  {
-    // validate .env
-    if (!BRANCH_CODE || !ACCOUNT_NUMBER || !PASSWORD) {
-        console.log('Invalid Try. Make sure to create a file ".env" and write your BRANCH_CODE / ACCOUNT_NUMBER / PASSWORD')
-        return
-    }
+  // validate .env
+  if (!BRANCH_CODE || !ACCOUNT_NUMBER || !PASSWORD) {
+    console.log('Invalid Try. Make sure to create a file ".env" and write your BRANCH_CODE / ACCOUNT_NUMBER / PASSWORD')
+    return
+  }
 
-    // launch browser
-    console.log('launching browser...')
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox']
-    })
+  // launch browser
+  console.log('launching browser...')
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  })
 
-    // go to page
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1440, height: 2000 })
-    await page.setExtraHTTPHeaders({
-        'Accept-Language': 'ja'
-    })
-    const LOGIN_URL = 'https://direct.smbc.co.jp/aib/aibgsjsw5001.jsp'
-    await page.goto(LOGIN_URL, { waitUntil: 'domcontentloaded' })
+  // go to page
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1440, height: 2000 })
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'ja'
+  })
+  const LOGIN_URL = 'https://direct.smbc.co.jp/aib/aibgsjsw5001.jsp'
+  await page.goto(LOGIN_URL, { waitUntil: 'domcontentloaded' })
 
-    // set input-data then submit
-    await page.type('input[name=S_BRANCH_CD]', BRANCH_CODE)
-    await page.type('input[name=S_ACCNT_NO]', ACCOUNT_NUMBER)
-    await page.type('input[name=PASSWORD]', PASSWORD)
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        page.click('input[type=submit]')
-    ]).catch(async err => {
-        console.log(err.response)
-        process.exit(1)
-    })
+  // set input-data then submit
+  await page.type('input[name=S_BRANCH_CD]', BRANCH_CODE)
+  await page.type('input[name=S_ACCNT_NO]', ACCOUNT_NUMBER)
+  await page.type('input[name=PASSWORD]', PASSWORD)
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.click('input[type=submit]')
+  ]).catch(async err => {
+    console.log(err.response)
+    process.exit(1)
+  })
 
-    // redirect to balance-page then get balance
-    await page.goto('https://direct3.smbc.co.jp/servlet/com.smbc.SUPRedirectServlet')
-    const balance = await page.$eval('.fRight', elm => elm.textContent.replace(/\s/g, ''))
-          .catch(async err => {
-              console.log(err)
-              process.exit(1)
-          })
-    console.log(`Your current balance is ${balance}`)
+  // redirect to balance-page then get balance
+  await page.goto('https://direct3.smbc.co.jp/servlet/com.smbc.SUPRedirectServlet')
+  const balance = await page.$eval('.fRight', elm => elm.textContent.replace(/\s/g, ''))
+        .catch(async err => {
+          console.log(err)
+          process.exit(1)
+        })
+  console.log(`Your current balance is ${balance}`)
 
-    // close browser
-    await browser.close()
+  // close browser
+  await browser.close()
   return balance
 }
